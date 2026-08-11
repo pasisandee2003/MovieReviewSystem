f4da18c875e3286d52c8c630a7f0e75f7babd188
pasisandee2003 <pasindu20bandara@gmail.com>
2026-08-12 00:55:51 +0530

    Add project files.

---
+++ MovieReviewAPI/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieReviewAPI.Models;
+
+namespace MovieReviewAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ MovieReviewAPI/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿namespace MovieReviewAPI.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string Genre { get; set; } = string.Empty;
+
+        public string Director { get; set; } = string.Empty;
+
+        public int ReleaseYear { get; set; }
+
+        public string PosterUrl { get; set; } = string.Empty;
+    }
+}
